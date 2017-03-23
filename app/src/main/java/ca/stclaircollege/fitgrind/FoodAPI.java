9ccfe049c71e4fc95317751cc36eca4f44689584
Johnny Nguyen <johnny.nguyen39@stclairconnect.ca>
2017-03-23 11:31:24 -0400

    #7: Implemented search method using the async search library.

--- app/src/main/java/ca/stclaircollege/fitgrind/FoodAPI.java
+++ app/src/main/java/ca/stclaircollege/fitgrind/FoodAPI.java
@@ -1,6 +1,13 @@
 package ca.stclaircollege.fitgrind;
 
-import android.content.Context;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import cz.msebera.android.httpclient.Header;
 
 /**
  * FoodAPI class is where we retrieve all our food, nutiritional values and much more.
@@ -12,8 +19,9 @@ public class FoodAPI {
     // We want to create constant URLS so we don't mess up.
     // URL Search is the URL needed for searching for a certain food item.
     // URL INFO is to get nutritional info from the search parameters.
-    private static final String URL_SEARCH = "https://api.nal.usda.gov/ndb/search/";
+    private static final String URL_SEARCH = "https://api.nal.usda.gov/ndb/search/?format=json";
     private static final String URL_INFO = "";
+    public static int MAX_RESULTS = 150; // the max results. The API returns a limit of 150 items, but the search results could be more.
 
     // We will need the API key. We can use context to pass, but having it passed like this might be much better
     private String apiKey;
@@ -28,8 +36,49 @@ public class FoodAPI {
      * @param food
      * @return Food object, which should provide important items needed
      */
-    public Food searchFood(String food) {
-        return null;
+    public FoodStore searchFood(String food) {
+        FoodStore store = null;
+        // we want to retrieve the results, and we want to encode the URL params too
+        String urlSearch = URL_SEARCH + "&q=" + food + "&api_key=" + this.apiKey;
+        // from urlSearch get the encoded
+        String encodedSeach = encodeUrl(urlSearch);
+        // check if it doesn't return null, if it doesn't we can do an async search
+        if (encodedSeach != null) {
+            // We want to create an async http request object
+            AsyncHttpClient client = new AsyncHttpClient();
+            // we want a GET request, so we use the get method from async
+            client.get(urlSearch, new AsyncHttpResponseHandler() {
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                }
+            });
+        }
+
+        // finally return the object
+        return store;
+    }
+
+    /**
+     * This private method will help us encode URL links when POSTing or GETing requests
+     * @param url
+     * @return a String that's been properly encoded
+     */
+    private String encodeUrl(String url) {
+        String results = null;
+        // get the url, and encode the results
+        try {
+            // encode it in utf-8
+            results = URLEncoder.encode(url, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return results;
     }
 
 }
--- app/src/main/java/ca/stclaircollege/fitgrind/FoodStore.java
+++ app/src/main/java/ca/stclaircollege/fitgrind/FoodStore.java
@@ -1,9 +1,31 @@
 package ca.stclaircollege.fitgrind;
 
 /**
- * Created by web on 2017-03-23.
+ * FoodStore is to hold an array of food results that we have. It's nie to have in a different array list and check
+ * @author Johnny Nguyen
+ * @version 1.0
  */
 
 public class FoodStore {
-    
+    // our arraylist
+    private Food[] foodList;
+    private int totalSearchResults;
+
+    /**
+     * the Integer search results is how much the array should hold in a foodList. This is good because we don't have to use
+     * an arraylist, since we know what size of the array is. We're not wasting space.
+     * @param totalSearchResults
+     */
+    public FoodStore(int totalSearchResults) {
+        this.totalSearchResults = totalSearchResults;
+        // now we can make our food list items here
+        this.foodList = new Food[this.totalSearchResults];
+    }
+
+    public Food[] getFoodList() {
+        return this.foodList;
+    }
+
+
+
 }
