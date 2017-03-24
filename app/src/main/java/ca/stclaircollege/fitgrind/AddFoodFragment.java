a533e3e4b43c0dfb85c8fbeaf42cc6fcf448bfb6
Johnny Nguyen <johnny.nguyen39@stclairconnect.ca>
2017-03-24 12:43:58 -0400

    #5: Added a skeleton view of the RecyclerView class. Now we have some sort of template to work by.

--- app/src/main/java/ca/stclaircollege/fitgrind/AddFoodFragment.java
+++ app/src/main/java/ca/stclaircollege/fitgrind/AddFoodFragment.java
@@ -3,10 +3,15 @@ package ca.stclaircollege.fitgrind;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
 
@@ -36,6 +41,15 @@ public class AddFoodFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    // get the connections
+    private FloatingActionButton searchButton;
+    private EditText searchField;
+
+    // Recycler View
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     public AddFoodFragment() {
         // Required empty public constructor
@@ -74,7 +88,84 @@ public class AddFoodFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_food, container, false);
 
+        // connect from the layout
+        searchButton = (FloatingActionButton) view.findViewById(R.id.searchButton);
+        searchField = (EditText) view.findViewById(R.id.searchField);
+
+        // set-up the recycler view
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+
+        // we want to set a fixed size, we know the content won't change
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout
+        mLayoutManager = new LinearLayoutManager(this.getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // use the adapter we have
+        mAdapter = new MyAdapter(new String[]{"test", "sdfsdf", "sdfsbf"});
+        mRecyclerView.setAdapter(mAdapter);
+
         return view;
+    }
+
+    /**
+     * Our adapter class for RecyclerView. This handles the layout issues on what it needs to have.
+     */
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        private String[] mDataset;
+
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            private TextView mTextView;
+
+            public ViewHolder(View view) {
+                super(view);
+
+                mTextView = (TextView) view.findViewById(R.id.info_text);
+            }
+
+            public TextView getTextView() {
+                return this.mTextView;
+            }
+
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public MyAdapter(String[] myDataset) {
+            mDataset = myDataset;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                       int viewType) {
+            // create a new view
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.search_layout, parent, false);
+
+            // set the view's size, margins, paddings and layout parameters
+            ViewHolder vh = new ViewHolder(view);
+
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+//            holder.mTextView.setText(mDataset[position]);
+            holder.getTextView().setText(mDataset[position]);
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return mDataset.length;
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
