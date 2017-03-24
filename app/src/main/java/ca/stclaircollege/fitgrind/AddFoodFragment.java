ca0a85191b6d00fd383eb20c0511dbecbfdef678
Johnny Nguyen <johnny.nguyen39@stclairconnect.ca>
2017-03-23 20:14:52 -0400

    #2: Connected Fab button, which should open a new screen. #5: Created a new fragment called AddFoodFragment, primarily for searching food.

--- app/src/main/java/ca/stclaircollege/fitgrind/AddFoodFragment.java
+++ app/src/main/java/ca/stclaircollege/fitgrind/AddFoodFragment.java
@@ -3,7 +3,6 @@ package ca.stclaircollege.fitgrind;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link MainFragment.OnFragmentInteractionListener} interface
+ * {@link AddFoodFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link MainFragment#newInstance} factory method to
+ * Use the {@link AddFoodFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MainFragment extends Fragment {
+public class AddFoodFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,10 +29,9 @@ public class MainFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    // connect from the xml layout here
-    private FloatingActionButton fab;
-
-    public MainFragment() {}
+    public AddFoodFragment() {
+        // Required empty public constructor
+    }
 
     /**
      * Use this factory method to create a new instance of
@@ -41,11 +39,11 @@ public class MainFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment MainFragment.
+     * @return A new instance of fragment AddFoodFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static MainFragment newInstance(String param1, String param2) {
-        MainFragment fragment = new MainFragment();
+    public static AddFoodFragment newInstance(String param1, String param2) {
+        AddFoodFragment fragment = new AddFoodFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -66,19 +64,7 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
-
-        // connect layout
-        fab = (FloatingActionButton) view.findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        return view;
+        return inflater.inflate(R.layout.fragment_add_food, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
