7b920db47b1402bcabbf43c5ec90f530bf9f99fc
Johnny Nguyen <jnguyen1236@gmail.com>
2017-04-02 02:05:04 -0400

    #1: Finished insert and update methods for workouts, and added part of select methods.

--- app/src/main/java/ca/stclaircollege/fitgrind/database/DatabaseHandler.java
+++ app/src/main/java/ca/stclaircollege/fitgrind/database/DatabaseHandler.java
@@ -2,8 +2,11 @@ package ca.stclaircollege.fitgrind.database;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
 
 /**
  * DatabaseClassHandler class.
@@ -24,6 +27,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     private static final String EXERCISE_TABLE_NAME = "exercise";
     private static final String CARDIOLOG_TABLE_NAME = "cardio_log";
     private static final String STRENGTHLOG_TABLE_NAME = "strength_log";
+    private static final String WORKOUT_TABLE_NAME = "workout";
 
     // create our table names
     private static final String CREATE_WEIGHTLOG_TABLE =
@@ -145,7 +149,6 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + CREATE_CARDIOLOG_TABLE);
         db.execSQL("DROP TABLE IF EXISTS " + CREATE_STRENGTHLOG_TABLE);
         db.execSQL("DROP TABLE IF EXISTS " + CREATE_WORKOUT_TABLE);
-
         // relaunch onCreate
         onCreate(db);
     }
@@ -171,7 +174,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
      * Inserts a workout, with the cardio object
      * @param cardio
      */
-    public void insertWorkout(Cardio cardio) {
+    public void insertWorkout(Cardio cardio, long routineId, long dayId) {
         // writeable db
         SQLiteDatabase db = getWritableDatabase();
         // create content values
@@ -185,13 +188,19 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         values.put("exercise_id", id);
         values.put("time", cardio.getTime());
         db.insert(CARDIOLOG_TABLE_NAME, null, values);
+        // now we finally want to insert the final workout
+        values.clear();
+        values.put("routine_id", routineId);
+        values.put("exercise_id", id);
+        values.put("day_id", dayId);
+        db.insert(WORKOUT_TABLE_NAME, null, values);
     }
 
     /**
      * Inserts workout with strength object
      * @param strength
      */
-    public void insertWorkout(Strength strength) {
+    public void insertWorkout(Strength strength, long routineId, long dayId) {
         // create db
         SQLiteDatabase db = getWritableDatabase();
         // create the content values
@@ -199,15 +208,132 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         // input the name and insert.
         values.put("name", strength.getName());
         // insert and retrieve the id
-        long id = db.insert(STRENGTHLOG_TABLE_NAME, null, values);
+        long id = db.insert(EXERCISE_TABLE_NAME, null, values);
         values.clear();
         // insert again
         values.put("exercise_id", id);
         values.put("set", strength.getSet());
         values.put("rep", strength.getReptitions());
         values.put("weight", strength.getWeight());
+        db.insert(STRENGTHLOG_TABLE_NAME, null, values);
+        // now we finally want to insert the final workout
+        values.clear();
+        values.put("routine_id", routineId);
+        values.put("exercise_id", id);
+        values.put("day_id", dayId);
+        db.insert(WORKOUT_TABLE_NAME, null, values);
     }
 
-    
+    /**
+     * Updates specific row
+     * @param routine
+     */
+    public void updateRoutine(Routine routine) {
+        // create db
+        SQLiteDatabase db = getWritableDatabase();
+        // setup content values
+        ContentValues values = new ContentValues();
+        // update the values
+        values.put("name", routine.getName());
+        values.put("description", routine.getDescription());
+        // update the db
+        db.update(WORKOUTROUTINE_TABLE_NAME, values, "id = ?", new String[]{String.valueOf(routine.getId())});
+    }
 
+    /**
+     * Updates with the cardio table
+     * @param cardio
+     * @param exerciseId
+     */
+    public void updateWorkout(Cardio cardio, long exerciseId) {
+        // writeable db
+        SQLiteDatabase db = getWritableDatabase();
+        // create content values
+        ContentValues values = new ContentValues();
+        // input the values
+        values.put("name", cardio.getName());
+        // update db
+        db.update(EXERCISE_TABLE_NAME, values, "id = ?", new String[]{String.valueOf(cardio.getId())});
+        // now update on this workout
+        values.clear(); // clear
+        values.put("exercise_id", exerciseId);
+        values.put("time", cardio.getTime());
+        db.update(CARDIOLOG_TABLE_NAME, values, "exercise_id = ?", new String[]{String.valueOf(exerciseId)});
+    }
+
+    /**
+     * Updates with the strength table
+     * @param strength
+     * @param exerciseId
+     */
+    public void updateWorkout(Strength strength, long exerciseId) {
+        // create db
+        SQLiteDatabase db = getWritableDatabase();
+        // create the content values
+        ContentValues values = new ContentValues();
+        // input the name and insert.
+        values.put("name", strength.getName());
+        // insert and retrieve the id
+        db.update(EXERCISE_TABLE_NAME, values, "id = ?", new String[]{String.valueOf(strength.getId())});
+        // update on this field
+        values.clear();
+        values.put("exercise_id", exerciseId);
+        values.put("set", strength.getSet());
+        values.put("rep", strength.getReptitions());
+        values.put("weight", strength.getWeight());
+        db.update(STRENGTHLOG_TABLE_NAME, values, "exercise_id = ?", new String[]{exerciseId});
+    }
+
+    /**
+     * delete routine
+     * @param id
+     */
+    public void deleteRoutine(long id) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.delete(WORKOUTROUTINE_TABLE_NAME, "id = ?", new String[]{String.valueOf(id)});
+    }
+
+    /**
+     * Deletes the cardio workout from 3 tables
+     * @param id
+     */
+    public void deleteCardioWorkout(long id) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.delete(CREATE_WORKOUT_TABLE, "exercise_id = ?", new String[]{String.valueOf(id)});
+        db.delete(CARDIOLOG_TABLE_NAME, "exercise_id = ?", new String[]{String.valueOf(id)});
+        db.delete(EXERCISE_TABLE_NAME, "id = ?", new String[]{String.valueOf(id)});
+    }
+
+    /**
+     * Deletes the strength/weights workout from 3 tables.
+     * @param id
+     */
+    public void deleteStrengthWorkout(long id) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.delete(CREATE_WORKOUT_TABLE, "exercise_id = ?", new String[]{String.valueOf(id)});
+        db.delete(STRENGTHLOG_TABLE_NAME, "exercise_id = ?", new String[]{String.valueOf(id)});
+        db.delete(EXERCISE_TABLE_NAME, "id = ?", new String[]{String.valueOf(id)});
+    }
+
+    // Get select All from
+
+    public ArrayList<Routine> selectAllRoutine() {
+        // get readable db
+        SQLiteDatabase db = getReadableDatabase();
+        // create results
+        ArrayList<Routine> results = null;
+        // CREATE A QUERY
+        Cursor cursor = db.rawQuery("SELECT * FROM " + WORKOUTROUTINE_TABLE_NAME, null);
+        if (cursor.moveToFirst()) {
+            results = new ArrayList<Routine>();
+            // go through a do-while
+            do {
+                // get the required string
+                results.add(new Routine(cursor.getInt(0), cursor.getString(1), cursor.getString(2)));
+            } while (cursor.moveToNext());
+        }
+        // return specified results
+        return results;
+    }
+    
 }
