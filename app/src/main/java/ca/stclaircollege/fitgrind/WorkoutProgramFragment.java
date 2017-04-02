142cfa877d81232abe995c39e3a09bafd459167d
Allan Lin <allan.lin15@stclairconnect.ca>
2017-04-02 02:49:23 -0400

    #4: added listview to exercise fragment

--- app/src/main/java/ca/stclaircollege/fitgrind/WorkoutProgramFragment.java
+++ app/src/main/java/ca/stclaircollege/fitgrind/WorkoutProgramFragment.java
@@ -72,7 +72,7 @@ public class WorkoutProgramFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_workout, container, false);
+        View view = inflater.inflate(R.layout.fragment_workout_program, container, false);
         list = (ListView) view.findViewById(R.id.workoutList);
         final ArrayList<Program> programsList = new ArrayList<Program>();
         programsList.add(new Program("text", "test", "test"));
