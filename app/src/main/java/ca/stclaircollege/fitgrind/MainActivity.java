99b1f148beecd372c472ffbee85ee5408dcee804
Johnny Nguyen <johnny.nguyen39@stclairconnect.ca>
2017-04-06 11:02:13 -0400

    #9: Added Key Value arrays, along with a settings app bar for android. Modified manifest to get the parent class when pressing back on the home button up

--- app/src/main/java/ca/stclaircollege/fitgrind/MainActivity.java
+++ app/src/main/java/ca/stclaircollege/fitgrind/MainActivity.java
@@ -75,7 +75,9 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            // setup intent
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
--- app/src/main/java/ca/stclaircollege/fitgrind/SettingsActivity.java
+++ app/src/main/java/ca/stclaircollege/fitgrind/SettingsActivity.java
@@ -1,31 +1,27 @@
 package ca.stclaircollege.fitgrind;
 
-import android.app.ActionBar;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 
-public class SettingsActivity extends PreferenceActivity {
+// we want to use AppCompat to support Actionbar, the Preference is being used for preferences on the fragment instead
+// so we are ok
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setupActionBar();
+
+        // get the action bar
+        ActionBar bar = getSupportActionBar();
+        bar.setDisplayHomeAsUpEnabled(true);
 
         // Display the fragment as the main content.
         getFragmentManager().beginTransaction()
-                .replace(R.id.content_main, new SettingsFragment())
+                .replace(android.R.id.content, new SettingsFragment())
                 .commit();
     }
-
-    private void setupActionBar() {
-        ActionBar actionBar = getActionBar();
-        if (actionBar != null) {
-            // Show the Up button in the action bar.
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-    }
-
 
     // create a fragment class inside of activity
     public static class SettingsFragment extends PreferenceFragment {
