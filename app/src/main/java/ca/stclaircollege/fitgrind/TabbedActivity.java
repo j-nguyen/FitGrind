35750188b0d1a2e8826f332440d798649454d690
Allan Lin <allan.lin15@stclairconnect.ca>
2017-04-07 11:52:40 -0400

    #4: remove fab button from the tabbed activity to the fragment

--- app/src/main/java/ca/stclaircollege/fitgrind/TabbedActivity.java
+++ app/src/main/java/ca/stclaircollege/fitgrind/TabbedActivity.java
@@ -72,16 +72,6 @@ public class TabbedActivity extends AppCompatActivity implements
         tabLayout.setupWithViewPager(mViewPager);
         setupTabIcons();
 
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
     }
 
     private void setupTabIcons() {
