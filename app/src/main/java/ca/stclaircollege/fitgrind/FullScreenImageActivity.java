864adc20c37fc4450f390b2db150c6400af760e9
Johnny Nguyen <johnny.nguyen39@stclairconnect.ca>
2017-04-14 15:59:30 -0400

    #6: added imageview settings to the picture

--- app/src/main/java/ca/stclaircollege/fitgrind/FullScreenImageActivity.java
+++ app/src/main/java/ca/stclaircollege/fitgrind/FullScreenImageActivity.java
@@ -1,8 +1,17 @@
 package ca.stclaircollege.fitgrind;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+
+import ca.stclaircollege.fitgrind.database.Progress;
 
 public class FullScreenImageActivity extends AppCompatActivity {
 
@@ -12,14 +21,20 @@ public class FullScreenImageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_screen_image);
+        mImageView = (ImageView) findViewById(R.id.full_image);
 
         // get intent retrieved data
         Bundle extras = getIntent().getExtras();
-        // check if an intent has paassed
+        // check if an intent has passed
         if (extras != null) {
+            Progress progress = extras.getParcelable("progress");
             // if it does set the new imageview
-            mImageView = (ImageView) findViewById(R.id.full_image);
-            mImageView.setImageBitmap();
+            // load with picasso
+//            System.out.println(progress.getResource());
+//            File file = new File(progress.getResource());
+//            Picasso.with(this).load(file).into(mImageView);
+            Bitmap image = BitmapFactory.decodeFile(progress.getResource());
+            mImageView.setImageBitmap(image);
         }
     }
 }
