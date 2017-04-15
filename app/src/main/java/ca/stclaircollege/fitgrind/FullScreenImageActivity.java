23e40b3447cb7180d80601184ca93c184d01af0b
Johnny Nguyen <johnny.nguyen39@stclairconnect.ca>
2017-04-15 18:51:54 -0400

    #6: Added trash icon on action bar, to delete the image.

--- app/src/main/java/ca/stclaircollege/fitgrind/FullScreenImageActivity.java
+++ app/src/main/java/ca/stclaircollege/fitgrind/FullScreenImageActivity.java
@@ -1,18 +1,19 @@
 package ca.stclaircollege.fitgrind;
 
-
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
+import com.github.chrisbanes.photoview.PhotoView;
 import com.squareup.picasso.Picasso;
 import java.io.File;
 import ca.stclaircollege.fitgrind.database.Progress;
 
 public class FullScreenImageActivity extends AppCompatActivity {
 
-    private ImageView mImageView;
+    private PhotoView mImageView;
+    private Progress progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +22,17 @@ public class FullScreenImageActivity extends AppCompatActivity {
 
         // get the action bar
         ActionBar bar = getSupportActionBar();
+        // create a back button for the action bar
         bar.setDisplayHomeAsUpEnabled(true);
 
-        mImageView = (ImageView) findViewById(R.id.full_image);
+        mImageView = (PhotoView) findViewById(R.id.full_image);
 
         // get intent retrieved data
         Bundle extras = getIntent().getExtras();
+
         // check if an intent has passed
         if (extras != null) {
-            Progress progress = extras.getParcelable("progress");
+            progress = extras.getParcelable("progress");
             // if it does set the new imageview
             File file = new File(progress.getResource());
             Picasso.with(this).load(file).into(mImageView);
@@ -37,9 +40,26 @@ public class FullScreenImageActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.image_menu, menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) finish();
+
+        // retrieve item
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            finish();
+        } else if (id == R.id.action_delete) {
+            // if it's delete then we need to delete
+            System.out.println(progress.getId()); // print out id)
+        }
 
         return super.onOptionsItemSelected(item);
     }
+
 }
