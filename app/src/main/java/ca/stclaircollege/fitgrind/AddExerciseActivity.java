89ff125f076649d001908c3c14c14f8a85439a43
Allan Lin <allan.lin15@stclairconnect.ca>
2017-04-15 23:50:35 -0400

    #4: add back button to activity

--- app/src/main/java/ca/stclaircollege/fitgrind/AddExerciseActivity.java
+++ app/src/main/java/ca/stclaircollege/fitgrind/AddExerciseActivity.java
@@ -3,6 +3,8 @@ package ca.stclaircollege.fitgrind;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class AddExerciseActivity extends AppCompatActivity implements
     AddExerciseFragment.OnFragmentInteractionListener,
@@ -12,9 +14,25 @@ public class AddExerciseActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_exercise);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // get the fragment manager
         getSupportFragmentManager().beginTransaction().replace(R.id.activity_add_exercise, new AddExerciseFragment()).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
